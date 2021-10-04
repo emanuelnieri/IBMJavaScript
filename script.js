42747d3d425a6763c67cf2a3a9e99d99ed887dc9
Emanuel Nieri <emanuel.nieri@gmail.com>
2021-10-04 17:27:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus;
+        document.getElementById("result").innerHTML="";
+        return false;
+    }
+        
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="<br>If you deposit <mark>"+ principal + "</mark>,<br> at an interest rate of <mark>" + rate+ ".</mark><br> You will receive an amount of interest <mark>" + interest +",</mark><br> in the year <mark>" + year ;
+    
+}
+
+function updateRate() 
+    {   
+    var rateval = document.getElementById("val").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+       
